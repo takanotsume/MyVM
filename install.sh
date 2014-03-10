7168be61f2dc86b1ef455573029f3287ac250271
msim <msim@rtbf.be>
2014-03-10 14:56:56 +0100

    State with apache2.4 and php 5.5

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+sudo apt-get remove virtualbox vagrant
+
 wget -P /tmp http://download.virtualbox.org/virtualbox/4.3.8/virtualbox-4.3_4.3.8-92456~Ubuntu~raring_amd64.deb
 wget -P /tmp http://download.virtualbox.org/virtualbox/4.3.8/VBoxGuestAdditions_4.3.8.iso
+wget -P /tmp https://dl.bintray.com/mitchellh/vagrant/vagrant_1.4.3_x86_64.deb
 sudo dpkg -i /tmp/virtualbox-4.3_4.3.8-92456~Ubuntu~raring_amd64.deb
+sudo dpkg -i /tmp/vagrant_1.4.3_x86_64.deb
+
 sudo mkdir /media/iso
 sudo mount -o loop /tmp/VBoxGuestAdditions_4.3.8.iso /media/iso
 
